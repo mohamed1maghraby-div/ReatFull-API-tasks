50dbf8431886f3d3bca9efeda77305460825f6a1
mohamed1maghraby <mohamed1maghraby@yahoo.com>
2019-06-04 06:23:47 +0200

    ADD edit 6/4

--- public/index.php
+++ public/index.php
@@ -11,4 +11,22 @@ $app->get('/hello/{name}', function (Request $request, Response $response, array
 
     return $response;
 });
+
+//post test
+$app->post('/test/demo', function(Request $r1, Response $r2){
+    $data= $r1->getParsedBody();
+    $inputdata=[];
+    $inputdata['name'] = filter_var($data['name'], FILTER_SANITIZE_STRING);
+    $inputdata['phone'] = filter_var($data['phone'], FILTER_SANITIZE_STRING);
+    $r2->getBody()->write("dear ".$inputdata['name']." your phone number is " .$inputdata['phone']);
+    return $response;
+});
+
+// get args
+$app->get('/args/{name}/{phone}', function($r1 , $r2, $args){
+    $name = $args['name'];
+    $phone = $args['phone'];
+    $r2->getBody()->write("dear ".$name." your phone number is " .$phone);
+    return $response;
+});
 $app->run();
